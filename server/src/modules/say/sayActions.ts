e8cff30365e080b75aafd2da0f5afa148af9c1ed
jerem2802 <creamedoc@gmail.com>
2024-12-15 18:06:37 +0100

    resolve04

--- server/src/modules/say/sayActions.ts
+++ server/src/modules/say/sayActions.ts
@@ -1,7 +1,8 @@
 import type { RequestHandler } from "express";
 
 const sayWelcome: RequestHandler = (req, res) => {
-  res.send("Welcome to the server!");
+  console.info(req.query);
+  res.send(`Welcome to the server!"${req.query.name}!`);
 };
 
 export default { sayWelcome };
--- server/src/router.ts
+++ server/src/router.ts
@@ -13,6 +13,8 @@ import itemActions from "./modules/item/itemActions";
 router.get("/api/items", itemActions.browse);
 router.get("/api/items/:id", itemActions.read);
 router.post("/api/items", itemActions.add);
+router.get("/api/programs/:id", programActions.read);
+router.get("/api/categories/:id", categoryActions.read);
 
 /* ************************************************************************* */
 import sayActions from "./modules/say/sayActions";
@@ -20,5 +22,8 @@ router.get("/", sayActions.sayWelcome);
 
 import programActions from "./modules/program/programActions";
 router.get("/api/programs", programActions.browse);
+
+import categoryActions from "./modules/category/categoryActions";
+router.get("/api/categories", categoryActions.browse);
 
 export default router;
